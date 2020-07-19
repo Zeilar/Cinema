d50e123cc241e539e882d1867ca7845cd0bd40cf
Philip <philip_angelin@hotmail.com>
2020-07-19 06:38:57 +0200

    -

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,11 @@ Route::post('/chat/is_typing', 'CommentController@isTyping')->name('chat_is_typi
 
 Route::middleware('throttle:10,1')->group(function () {
     Route::post('/comment/send', 'CommentController@store')->name('comment_send');
+    Route::post('/room/create', 'RoomController@createRoom')->name('create_room');
     Route::post('/video/change', 'VideoController@change')->name('video_change');
     Route::post('/video/reset', 'VideoController@reset')->name('video_reset');
     Route::post('/video/pause', 'VideoController@pause')->name('video_pause');
-    Route::post('/room/create', 'RoomController@createRoom')->name('create_room');
+    Route::get('/room/{id}', 'RoomController@enterRoom')->name('visit_room');
     Route::post('/video/play', 'VideoController@play')->name('video_play');
     Route::post('/video/sync', 'VideoController@sync')->name('video_sync');
-    Route::get('/room/{id}', 'RoomController@enterRoom')->name('visit_room');
 });
