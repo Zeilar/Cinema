9db24e97b668d04bb0b9ea11cafbb14a4bf18dee
Philip <philip_angelin@hotmail.com>
2020-07-20 05:30:04 +0200

    Accidentally removed webpack

---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.react('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
+
+// Copy into dist folder for production
+if (mix.inProduction()) {
+	// App
+	mix.copyDirectory('app', 'dist/cinema/app');
+	mix.copyDirectory('config', 'dist/cinema/config');
+	mix.copyDirectory('database', 'dist/cinema/database');
+	mix.copyDirectory('resources/views', 'dist/cinema/resources/views');
+	mix.copyDirectory('routes', 'dist/cinema/routes');
+    mix.copyDirectory('storage/app/public', 'dist/cinema/storage/app/public');
+	mix.copyDirectory('tests', 'dist/cinema/tests');
+
+	// Public
+	mix.copyDirectory('public/css', 'dist/cinema.angelin.dev/css');
+	mix.copyDirectory('public/js', 'dist/cinema.angelin.dev/js');
+}
